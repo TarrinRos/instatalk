24441d111bb1e17f471e32f3fed3ed8498a30259
TarrinRos <tarindis@gmail.com>
2019-02-07 20:00:22 +0200

    Add settings for room channels

---
+++ app/channels/room_channel.rb
@@ -0,0 +1,21 @@
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+    logger.info "Subscribed to RoomChannel, roomId: #{params[:roomId]}"
+
+    @room = Room.find(params[:roomId])
+
+    stream_from "room_channel_#{@room.id}"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+    logger.info 'Unsubscribed from RoomChannel'
+  end
+
+  def speak(data)
+    logger.info "Roomchannel, speak: #{data.inspect}"
+
+    ActionCable.server.broadcast "room_channel_#{@room.id}", message: data['message']
+  end
+end
